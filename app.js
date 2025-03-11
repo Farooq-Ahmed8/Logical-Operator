da0871c90e5a1d9c412f77fc7f8411b788498e67
Farooq Ahmed <03174748082ali@gmail.com>
2025-03-11 23:58:21 +0500

    complete

--- app.js
+++ app.js
@@ -4,12 +4,12 @@
 
 //6 > 5 && 6 < 5 + 20 / 4 === 20 && 50 !== 50 + 50 !== '50';
 //true && false + true && false + true && 
-//document.write(true && false && true && fasle && true)
+document.write('<h3> ', true && false && true && fasle && true, '</h3>')
 //false
 
 //10 >= 10 && 20 !== '20' && 90 < 100 && 20 != 20 
 //true && true && true && true && false
-//document.write(true && true && true && true && false)
+document.write('<h3> ', true && true && true && true && false, '</h3>')
 //false
 
 
@@ -19,5 +19,5 @@
 //30 === 30 || 20 > 25 || 60 !== 60 || 90 < 91 || 50 == 50
 //true || false || false || true || true
 //true
-document.write(true || false || false || true || true)
+document.write('<h3> ', true || false || false || true || true, '</h3>')
 
