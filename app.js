99aaa1acade57057ad52190e75f879f480f37a23
Farooq Ahmed <03174748082ali@gmail.com>
2025-03-11 23:55:10 +0500

    completed Logical Operator

---
+++ app.js
@@ -0,0 +1,23 @@
+//logical Operator
+
+//&& == and
+
+//6 > 5 && 6 < 5 + 20 / 4 === 20 && 50 !== 50 + 50 !== '50';
+//true && false + true && false + true && 
+//document.write(true && false && true && fasle && true)
+//false
+
+//10 >= 10 && 20 !== '20' && 90 < 100 && 20 != 20 
+//true && true && true && true && false
+//document.write(true && true && true && true && false)
+//false
+
+
+
+//|| == OR
+
+//30 === 30 || 20 > 25 || 60 !== 60 || 90 < 91 || 50 == 50
+//true || false || false || true || true
+//true
+document.write(true || false || false || true || true)
+
